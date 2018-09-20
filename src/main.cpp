a5b1e6cdeba8d958e87374a3648dafb0a31869cf
Lucas <lucasleitemm@hotmail.com>
2018-09-20 02:23:47 -0300

    Aprimoramentos na função de votar em deputado federal

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,82 @@ using namespace std;
 
 Candidato candidato[1238];
 
+///////////////----------INICIO DAS FUNÇÕES---------///////////////////////
+
+
+//____Função para votar em deputado federal:____//
+
+void VotarDeputadoFed(){
+
+//Lendo codigo do candidato:
+
+
+	cout<<"Deputado Federal :"<<endl;
+	string codigo_cand_depfed;
+
+	int i;
+
+	do{
+		cin >> codigo_cand_depfed;
+
+		if(codigo_cand_depfed.size()!=4){
+
+			cout << "Insira 4 digitos !"<<endl;
+		}
+
+	}while(codigo_cand_depfed.size()!=4);
+
+		
+	for(i=0;i<=1236;i++){
+
+		if(codigo_cand_depfed == candidato[i].numero){
+
+			if(candidato[i].cargo == "DEPUTADO FEDERAL") {
+
+				cout<<"Nome: "<< candidato[i].nome_candidato << endl;
+				cout<<"Regiao: "<< candidato[i].regiao << endl;
+				cout<<"Cargo: "<< candidato[i].cargo << endl;
+				cout<<"Partido: "<< candidato[i].nome_partido << endl;
+				cout<<"Numero do partido: "<<candidato[i].numero_partido << endl;
+
+
+
+			}
+
+
+		}
+
+
+			else{
+
+				cout<<"Candidato Inexistente!"<<endl;
+
+				break;
+
+			}
+
+
+	}
+
+}
+
+
+/////////////////--------FIM DAS FUNÇÕES E COMEÇO DA MAIN--------/////////////////////
+
+
 int main() {
 
 
 
 	int i;
 
-	// ABERTURA DO ARQUIVO DOS CANDIDATOS(APENAS DF)
+	// ABERTURA DO ARQUIVO DOS CANDIDATOS(APENAS DF):
 	
 
 	ifstream cp("./data/cand_2018_DF.csv");
 
 
-	// ERRO CASO A ABERTURA FALHE 
+	// ERRO CASO A ABERTURA FALHE :
 
 	if(!cp.is_open()){
 
@@ -71,37 +134,11 @@ int main() {
 	cp.close();
 
 	
-//Lendo codigo do candidato:
 
 
-	cout<<"Deputado Federal :"<<endl;
-	string codigo_cand;
+// Funções para votação:
 
-	cin >> codigo_cand;
-
-		
-	for(i=0;i<=1236;i++){
-
-		if(codigo_cand == candidato[i].numero){
-
-			if(candidato[i].cargo == "DEPUTADO FEDERAL") {
-
-				cout<<"Nome do candidato: "<< candidato[i].nome_candidato << endl;
-
-
-			}
-			else{
-
-				cout<<"Inexistente"<<endl;
-
-			}
-
-
-
-		}
-
-	}
-
+	VotarDeputadoFed();
 
 
 
